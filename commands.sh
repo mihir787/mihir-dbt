01f7705a5fc86c4c90564b387e2c3e1f1ac7d67a
mihir787 <mihir787@gmail.com>
2019-04-02 23:30:01 -0600

    added some commands

--- commands.sh
+++ commands.sh
@@ -5,3 +5,8 @@ docker images #top id
 docker run -i -t 0d2c8c15d61e /bin/bash
 
 dbt run --profiles-dir .dbt
+
+$(aws ecr get-login --no-include-email --region us-east-1) #login
+
+docker tag mihir-dbt:latest 335090205743.dkr.ecr.us-east-1.amazonaws.com/mihir/dbt-project:latest
+docker push 335090205743.dkr.ecr.us-east-1.amazonaws.com/mihir/dbt-project:latest
